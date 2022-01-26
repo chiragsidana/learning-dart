c5dd5857ba69d10820be66cba752375dcc7b1c6f
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-26 21:07:00 +0530

    string intro

---
+++ string.dart
@@ -0,0 +1,16 @@
+main(){
+  var s1='kuch bhi';
+  var s2="kuch bhi part 2";
+  var s3='it\'s easy ';
+  var s4="it's zyada easy";
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  var s=r'\n';
+  print(s);
+  var s5=''' multi
+  lines string''';
+  //double ko 3 baar se bhi hojayega
+  print(s5);
+}
