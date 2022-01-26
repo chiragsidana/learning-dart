20f1c2c7b30c76c45e9d4cb854952631b8f0edb7
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-26 20:39:42 +0530

    data types

---
+++ data_types.dart
@@ -0,0 +1,27 @@
+main(){
+  int amount1=100;
+  var amount2=200;
+
+  print('Amount1:$amount1 | Amount2: $amount2 \n');
+
+  double damount1=100.11;
+  var damount2=200.22;
+
+  print('damount1: $damount1 | damount2: $damount2 \n');
+
+  String name1='Chirag';
+  var name2='Sidana';
+
+  print('My name is $name1 $name2 \n');
+
+  bool istrue1=true;
+  var istrue2=false;
+
+  print('istrue1 is $istrue1 | istrue2 is $istrue2\n');
+
+  dynamic weakvariable=100;
+  print('WeakVariable 1: $weakvariable\n');
+
+  weakvariable='Dart';
+  print('weakvariable 2: $weakvariable\n');
+}
