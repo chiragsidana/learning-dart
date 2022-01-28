921d08ed534843e9f574063b335c7b84b6cdf968
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-28 22:28:53 +0530

    string conversion

--- string_conversion.dart
+++ string_conversion.dart
@@ -3,4 +3,8 @@ main(){
   assert(one==1);
   var onepointone=double.parse('1.1');
   assert(onepointone==1.1);
+  String oneasString=1.toString();
+  assert(oneasString=='1');
+  String piasString=3.14159.toStringAsFixed(2);
+  assert(piasString=='3.14');
 }
