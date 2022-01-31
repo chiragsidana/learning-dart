554f5ed53a0d80c33fadb4dec47931b07ed5b1f1
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-31 13:04:33 +0530

    map

---
+++ collection_map.dart
@@ -0,0 +1,13 @@
+void main(){
+  var gifts={
+    //key :    Value
+    'first': 'chirag',
+    2: 'sidana',
+    'third': 'rasik'
+  };
+  print(gifts['third']);
+  print(gifts[2]);
+  var gifts2=Map();
+  gifts2[1]='mango';
+  print(gifts2[1]);
+}
