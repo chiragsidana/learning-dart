1f99e72da913fd08dc0762682a69c6d5bdcba9b7
ChiragSidana <chiragsidana2709@gmail.com>
2022-01-31 12:46:02 +0530

    set

---
+++ collection_set.dart
@@ -0,0 +1,9 @@
+void main(){
+  // ignore: equal_elements_in_set
+  var halogems={'chirag','sidana','chirag'};
+  for(var x in halogems){
+    print(x);
+  }
+  // ignore: unused_local_variable
+  Set<String> names={};
+}
